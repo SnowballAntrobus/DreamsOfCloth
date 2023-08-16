6e39fe03e24fee98f59112baf65f74fda4596ccb
Dante Gil-Marin <drgilmarin@icloud.com>
2023-08-15 20:14:57 -0700

    Initial Commit

---
+++ DreamsOfCloth/DreamsOfClothApp.swift
@@ -0,0 +1,17 @@
+//
+//  DreamsOfClothApp.swift
+//  DreamsOfCloth
+//
+//  Created by Dante Gil-Marin on 8/15/23.
+//
+
+import SwiftUI
+
+@main
+struct DreamsOfClothApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
